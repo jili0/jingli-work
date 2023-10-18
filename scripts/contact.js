0b0b027697c9b89b2272d7abded4329e766aeab3
Jing Li <jingli0319gmail.com>
2023-10-18 11:48:14 +0200

    improve contact page

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,6 +1,11 @@
-function onSubmit() {
-    const yourName = document.querySelector('#name').value;
-    const yourMessage = document.querySelector('#message').value;
-    localStorage.setItem(yourName, yourName);
-    localStorage.setItem(yourMessage, yourMessage);
-}
+document.querySelector('#name').addEventListener("change", function(){
+    let yourName = document.querySelector('#name').value;
+    let showName = document.querySelector('#showName');
+    showName.innerText = `Your name: ${yourName}`;
+});
+
+document.querySelector('#message').addEventListener("change", function(){
+    let yourMessage = document.querySelector('#message').value;
+    let showMessage = document.querySelector('#showMessage');
+    showMessage.innerText = `Your message: ${yourMessage}`;
+});
