18c148575a835848de7655d178c8496232cb9614
Jing Li <jingli0319gmail.com>
2023-10-20 21:59:33 +0200

    add remark to the comment page

--- scripts/comment.js
+++ scripts/comment.js
@@ -1,8 +1,24 @@
+let postRemark = document.querySelector('.postRemark');
+let remark = 'Remark: Hi, you can write and post some comments here. You will see a message box, which shows you the comment that you\'ve just post; And you can see all your comments listed in the Comment Board below, with the local time attached to them. There are also some default messages in the Comment Board. If you click "Clear local", however, all comments that you\'ve posted will be removed. This is because the comment messages are just saved in the localstorage of your computer. I\'m currently learning PHP and mySQL, and later the comments should be saved in a real Database. Let\'s see!';
+let text = '';
+function printNextLetter () {  
+        if (remark.length > text.length) {
+            let letterToPrint = remark[text.length];
+            postRemark.innerHTML += letterToPrint;
+            text += letterToPrint;
+        } 
+        setTimeout(printNextLetter, 50);
+}
+printNextLetter();
+
+
+
+
 let input = document.querySelector('#message');
 let newMessage = document.querySelector('.js-new-message');
 let send = document.querySelector('#send');
 let messageContainer =  document.querySelector('.js-message');
-let savedMessage = ["Why don't scientists trust atoms? Because they make up everything!",
+let defaultMessage = ["Why don't scientists trust atoms? Because they make up everything!",
 "Did you hear about the mathematician who's afraid of negative numbers? He'll stop at nothing to avoid them.",
 "Parallel lines have so much in common. It's a shame they'll never meet.",
 "How does a penguin build its house? Igloos it together!",
@@ -12,31 +28,20 @@ let savedMessage = ["Why don't scientists trust atoms? Because they make up ever
 
 
 
-for(let i=0; i<savedMessage.length; i++) {
-    messageContainer.innerHTML += `<p>${savedMessage[i]}<br>-------------</p>`;
-}
-
-
 function updateMessage() {
     messageContainer.innerHTML = '';
+
     let keys = Object.keys(localStorage);
-    let length = keys.length;
-    let storedItems = [];
     let date = '';
-    for (let i = 0; i < length; i++) {
-        let item = localStorage.getItem(keys[i]);
-        date = new Date(keys[i] / 1).toLocaleDateString() + ', ' +  new Date(keys[i] / 1).toLocaleTimeString();
-        storedItems.push(item); 
-        storedItems.sort();  
-        storedItems.reverse();                                                                                                         
-        console.log(`storedItems: ${storedItems}`);
-    }
-    for(let i=0; i<storedItems.length; i++) {
-        messageContainer.innerHTML += `<p>${storedItems[i]}<br>${date}<br>-------------</p>`;
-    }
-    for(let i=0; i<savedMessage.length; i++) {
+    keys.sort().reverse().forEach((key)=>{
+        date = new Date(key / 1).toLocaleDateString() + ', ' +  new Date(key / 1).toLocaleTimeString();
+        messageContainer.innerHTML += `<p>${localStorage.getItem(key)}<br>${date}<br>-------------</p>`;
+    });
+        
+   
+    for(let i=0; i<defaultMessage.length; i++) {
         let date = new Date().toLocaleDateString() + ', ' +new Date().toLocaleTimeString();
-        messageContainer.innerHTML += `<p>${savedMessage[i]}<br>${date}<br>-------------</p>`;
+        messageContainer.innerHTML += `<p>${defaultMessage[i]}<br>${date}<br>-------------</p>`;
     }
 }
 
@@ -47,7 +52,7 @@ send.addEventListener('click', ()=>{
     let date = new Date();
     let itemName = date.getTime();
     localStorage.setItem(itemName, input.value);
-    let message = localStorage.getItem(itemName);
+
     updateMessage();
     } else if (!input.value){
     newMessage.innerHTML = '<p>It seems you didn\'t write anything yet. Try again!';
