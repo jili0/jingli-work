877619b542d0a5328d1e76b397be5640b9d7fa21
Jing Li <jingli0319@gmail.com>
2023-12-01 01:01:07 +0100

    Add style to contact page

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 let index = 0;
-let info = "Are you looking for a Web Developer?<br> A team member? <br>Someone who helps you build your own unique website? <br>Yes! You&#39;re here right.";
+let info = "Are you looking for a Web Developer?<br> A team member? <br>Someone who helps you build your own unique website? <br>Yes! You&#39;re here right. 🚀";
 
 function printNextWord () {  
   let infoSection = document.querySelector('.js-info');
