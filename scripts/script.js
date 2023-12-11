c1c83e9e2d6ac3bcd98575229ac9ebd31dddc7dc
Jing Li <jingli0319@gmail.com>
2023-12-11 09:12:02 +0100

    Hide the sidebar when clicking the rest of page

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,21 @@
 const headerPicture = document.querySelector('.header__picture');
-
+const sidebar = document.querySelector('.sidebar');
 const toggleSidebar = () => {
-  const sidebar = document.querySelector('.sidebar');
   if (sidebar.style.right != "0px") {
     sidebar.style.right = "0px";
   } else {
     sidebar.style.right = "-10000px";
   }
 };
+headerPicture.addEventListener('click', toggleSidebar, false);
 
-headerPicture.addEventListener('click', toggleSidebar, false)
+const main = document.querySelector('main');
+const hideSidebar = () => {
+  if (sidebar.style.right = "0px") {
+    sidebar.style.right = "-10000px";
+  }
+};
+const test = () => {
+  console.log('test test');
+};
+main.addEventListener('click', hideSidebar, false);
