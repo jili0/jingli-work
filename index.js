df6533789489e2214050a307bc33ea654e6b7866
Jing <jingli0319@gmail.com>
2024-01-23 15:02:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const data = [
+  {
+    imgSrc: "images/portfolio_400_477.png",
+    title: "My Portfolio",
+    description: "This is my portfolio",
+    href: "https://jili0.github.io/portfolio/",
+  },
+  {
+    imgSrc: "images/MoodPadImg_800_800.png",
+    title: "MoodPad",
+    description:
+      "Each user, after logging in, can create, read, update and write his or her posts here. The user can also change the user name and the profile picture. Everything is saved in Firebase and can be synchronized.",
+    href: "https://jili0.github.io/portfolio/",
+  },
+  {
+    imgSrc: "https://picsum.photos/800/800",
+    title: "Jili Blog",
+    description: "I&apos;m still working on this project.",
+    href: "https://jiliblog.netlify.app/",
+  },
+  {
+    imgSrc: "https://picsum.photos/800/800",
+    title: "To do List",
+    description:
+      "A To-Do-List, writing in vanilla javascript. It&apos;s not online yet.",
+    href: "https://jili0.github.io/portfolio/",
+  },
+];
+
+const projectsContainer = document.querySelector(".projectsContainer");
+const projectsArray = data.map(
+  ({
+    imgSrc,
+    title,
+    description,
+    href,
+  }) => `<div class="col-12 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center position-relative">
+  <div class="card pb-5 my-5" style="width: 18rem">
+<img
+  src=${imgSrc}
+  class="card-img-top"
+  style="margin-bottom: 10vh;"
+  alt=${title}
+/>
+<div class="card-body position-absolute bottom-0 left-0 right-0 bg-light col-12">
+  <h5 class="card-title">${title}</h5>
+  <p class="card-text" style="height: 10vh; overflow: auto">
+  ${description}
+  </p>
+  <a class="btn btn-primary" href=${href}>Go to ${title}</a>
+</div>
+</div>
+</div>`
+);
+
+projectsContainer.innerHTML = projectsArray.join("");
